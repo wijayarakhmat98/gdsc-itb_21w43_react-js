c2df8d7ad371b3e839a184ff6d9dcc8e73a22dca
Wijaya Rakhmat <wijaya.rakhmat@hotmail.com>
2021-11-12 22:43:02 +0700

    Catalog

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css"
+import data from "./data.json"
+import Images from "./img.js"
+
+function Search() {
+	return (
+		<div className="search">
+			<p>SEARCH</p>
+		</div>
+	)
+}
+
+function Catalog() {
+	return (
+		<div className="catalog">
+			<div><p>CATALOG</p></div>
+			<div>
+			{
+				data.map((movie) => {
+					return (
+						<div className="movie" key={movie.title}>
+							<div className="cover">
+								<img src={Images[movie.cover]} alt={movie.alternative} />
+							</div>
+							<div className="title">
+								<p><strong>{movie.title}</strong></p>
+							</div>
+							<p>({movie.year})</p>
+						</div>
+					)
+				})
+			}
+			</div>
+		</div>
+	)
+}
+
+function Details() {
+	return (
+		<div className="details">
+			<div><p>DETAILS</p></div>
+		</div>
+	)
+}
+
+function Wishlist() {
+	return (
+		<div className="wishlist">
+			<div><p>WISHLIST</p></div>
+		</div>
+	)
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+		<div>
+			<Search />
+			<div className="wrapper">
+				<Wishlist />
+				<Catalog />
+				<Details />
+			</div>
+		</div>
+	)
 }
 
 export default App;
---
+++ src/img.js
@@ -0,0 +1,24 @@
+const images = {
+	"soul": require("./img/soul.jpg").default,
+	"whiplash": require("./img/whiplash.jpg").default,
+	"the lobster": require("./img/the lobster.jpg").default,
+	"the mauritanian": require("./img/the mauritanian.jpg").default,
+	"1917": require("./img/1917.jpg").default,
+	"ford v ferrari": require("./img/ford v ferrari.jpg").default,
+	"hacksaw ridge": require("./img/hacksaw ridge.jpg").default,
+	"taxi driver": require("./img/taxi driver.jpg").default,
+	"predestination": require("./img/predestination.jpg").default,
+	"memoirs of a geisha": require("./img/memoirs of a geisha.jpg").default,
+	"the boy in the striped pajamas": require("./img/the boy in the striped pajamas.jpg").default,
+	"little women": require("./img/little women.jpg").default,
+	"jojo rabbit": require("./img/jojo rabbit.jpg").default,
+	"redline": require("./img/redline.jpg").default,
+	"a dog's purpose": require("./img/a dog's purpose.jpg").default,
+	"free guy": require("./img/free guy.jpg").default,
+	"dune": require("./img/dune.jpg").default,
+	"coco": require("./img/coco.jpg").default,
+	"catch me if you can": require("./img/catch me if you can.jpg").default,
+	"blade runner 2049": require("./img/blade runner 2049.jpg").default
+}
+
+export default images
